5667a0cf13110d2381d007c25108716c39819f96
lans10 <123293870+lans10@users.noreply.github.com>
2024-08-01 14:57:59 -0400

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,31 @@
+with open("input.txt", "r") as f:
+    inp = f.readlines()
+d = {}
+def main():
+    points=0
+    for card in inp:
+        card = card.split(':')
+        num = ''.join(card[0]).split()[1]
+        card = ''.join(card[1])
+        card = card.split('|')
+        win = ''.join(card[0]).split()
+        elf = ''.join(card[1]).split()
+        match = 0
+        for n in win:
+            if n in elf:
+                match+=1
+        if match>0:
+            points+=2**(match-1)
+        d[num]=[match,1]
+    print(points)
+    
+    sc = 0
+    for key in d.keys():
+        #print("Card "+key+": "+str(d[key]))
+        for k in range(d[key][1]):
+            for i in range(1, d[key][0]+1):
+                d[str(int(key)+i)][1]+=1
+        sc+=d[key][1]
+    print(sc)
+
+main()
