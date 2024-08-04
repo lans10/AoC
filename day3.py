0956b4c965c5e8b9a0bd752fc65b98f4bdbd7086
lans10 <123293870+lans10@users.noreply.github.com>
2024-08-04 15:49:37 -0400

    Update day3.py

--- day3.py
+++ day3.py
@@ -6,46 +6,46 @@ for i in range(0, len(inp)):
     #print(inp[i])
 
 def main():
-    sum = 0
+    _sum = 0
     for i in range(0, len(inp)):
         for j in range(0,len(inp[i])):
             if inp[i][j].isdigit():
-                sum += checkAdj(inp, i,j, 1)
-    print(sum)
+                _sum += check_adj(inp, i,j, 1)
+    print(_sum)
     ratio = 0
     for key in d1.keys():
         if len(d1[key])==2:
             ratio+=d1[key][0]*d1[key][1]
     print(ratio)
-def checkAdj(l1, i,j, replace):
+def check_adj(l1, i,j, replace):
     num = l1[i][j]
-    tmpIndex=j+1
-    while tmpIndex<len(inp[i]):
-        if l1[i][tmpIndex].isdigit():
-            num+=l1[i][tmpIndex]
+    tmp_index=j+1
+    while tmp_index<len(inp[i]):
+        if l1[i][tmp_index].isdigit():
+            num+=l1[i][tmp_index]
         else:
             break
-        tmpIndex+=1
+        tmp_index+=1
     numsize = len(num)
     num = int(num)
     sym = 0
-    symX, symY = 0,0
+    sym_x, sym_y = 0,0
     for k in range(j,j+numsize):
         inp[i][k]='.'
     #left
     if j>0 and sym == 0:
         if l1[i][j-1]!='.':
             sym = 1
-            symX, symY = i, j-1
-            update(num, symX, symY)
+            sym_x, sym_y = i, j-1
+            update(num, sym_x, sym_y)
             #print(l1[i][j-1])
             #l1[i][j-1]='.'
     #top left
     if j>0 and i>0 and sym == 0:
         if l1[i-1][j-1]!='.':
             sym = 1
-            symX, symY = i-1, j-1
-            update(num, symX, symY)
+            sym_x, sym_y = i-1, j-1
+            update(num, sym_x, sym_y)
             #print(l1[i][j-1])
             #l1[i][j-1]='.'
     #top middle
@@ -53,16 +53,16 @@ def checkAdj(l1, i,j, replace):
         for ind in range(j,j+numsize):
             if l1[i-1][ind]!='.':
                 sym = 1
-                symX, symY = i-1, ind
-                update(num, symX, symY)
+                sym_x, sym_y = i-1, ind
+                update(num, sym_x, sym_y)
                 #print(l1[i-1][ind])
                 #l1[i-1][ind]='.'
                 break
     #top right
     if j+numsize<len(inp[i]) and i>0 and sym == 0:
         if l1[i-1][j+numsize]!='.':
-            symX, symY = i-1, j+numsize
-            update(num, symX, symY)
+            sym_x, sym_y = i-1, j+numsize
+            update(num, sym_x, sym_y)
             sym = 1
             #print(l1[i-1][j+numsize])
             #l1[i-1][j+numsize]='.'
@@ -70,16 +70,16 @@ def checkAdj(l1, i,j, replace):
     if j+numsize<len(inp[i]) and sym == 0:
         if l1[i][j+numsize]!='.':
             sym = 1
-            symX, symY = i, j+numsize
-            update(num, symX, symY)
+            sym_x, sym_y = i, j+numsize
+            update(num, sym_x, sym_y)
             #print(l1[i][j+numsize])
             #l1[i][j+numsize]='.'
     #bottom left
     if i+1<len(inp) and j>0 and sym == 0:
         if l1[i+1][j-1]!='.':
             sym = 1
-            symX, symY = i+1, j-1
-            update(num, symX, symY)
+            sym_x, sym_y = i+1, j-1
+            update(num, sym_x, sym_y)
             #print(l1[i+1][j-1])
             #l1[i+1][j-1]='.'
     #bottom middle
@@ -87,8 +87,8 @@ def checkAdj(l1, i,j, replace):
         for ind in range(j,j+numsize):
             if l1[i+1][ind]!='.':
                 sym = 1
-                symX, symY = i+1, ind
-                update(num, symX, symY)
+                sym_x, sym_y = i+1, ind
+                update(num, sym_x, sym_y)
                 #print(l1[i+1][ind])
                 #l1[i+1][ind]='.'
                 break
@@ -96,8 +96,8 @@ def checkAdj(l1, i,j, replace):
     if i+1<len(inp) and j+numsize<len(inp[i]) and sym == 0:
         if l1[i+1][j+numsize]!='.':
             sym = 1
-            symX, symY = i+1, j+numsize
-            update(num, symX, symY)
+            sym_x, sym_y = i+1, j+numsize
+            update(num, sym_x, sym_y)
             #print(l1[i+1][j+numsize])
             #l1[i+1][j+numsize]='.'
     if sym == 0:
