88efc4fadb6f9cde4a78a8ae27fe4e3678c148a9
lans10 <123293870+lans10@users.noreply.github.com>
2024-08-05 10:18:31 -0400

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,39 +1,45 @@
-#part one only
-with open("input.txt", "r") as f:
-    inp = f.read()
-inp = inp.split("\n")
-print(inp)
-
+"""
+Day 5 part one Advent of Code
+"""
 def extract(inp):
+    """
+    Returns mapping of seed to location for one seed
+    """
     #seeds
     pointer = 0
     seeds = inp[pointer].split(":")[1].split()
-    for i in range(len(seeds)):
-        seeds[i]=int(seeds[i])    
+    for i in enumerate(seeds):
+        seeds[i]=int(seeds[i])
     #seed to soil
     pointer = 3
-    seedsoil, pointer = extract_helper(pointer, inp)    
+    seedsoil, pointer = extract_helper(pointer, inp)
     #soil to fertilizer
     pointer +=2
-    soilfertilizer, pointer = extract_helper(pointer, inp)    
+    soilfertilizer, pointer = extract_helper(pointer, inp)
     #fertilizer to water
     pointer +=2
-    fertilizerwater, pointer = extract_helper(pointer, inp)    
+    fertilizerwater, pointer = extract_helper(pointer, inp)
     #water to light
     pointer +=2
-    waterlight, pointer = extract_helper(pointer, inp)    
+    waterlight, pointer = extract_helper(pointer, inp)
     #light to temp
     pointer +=2
-    lighttemp, pointer = extract_helper(pointer, inp)    
+    lighttemp, pointer = extract_helper(pointer, inp)
     #temp to humidity
     pointer +=2
-    temphumidity, pointer = extract_helper(pointer, inp)    
+    temphumidity, pointer = extract_helper(pointer, inp)
     #humidity to location
     pointer +=2
-    humiditylocation, pointer = extract_helper(pointer, inp)    
+    humiditylocation, pointer = extract_helper(pointer, inp)
     return seeds, seedsoil, soilfertilizer, fertilizerwater, waterlight, lighttemp, temphumidity, humiditylocation
     
 def main():
+    """
+    Reads file and prints locations per seed
+    """
+    with open("input.txt", "r", encoding="utf-8") as f:
+    inp = f.read()
+    inp = inp.split("\n")
     seeds, seedsoil, soilfertilizer, fertilizerwater, waterlight, lighttemp, temphumidity, humiditylocation = extract(inp)
     locs = []
     for seed in seeds:
@@ -48,23 +54,30 @@ def main():
     locs.sort()
     print(locs[0])
     return 0
-    
+
 def extract_helper(pointer, inp):
+    """
+    Helper function that helps map one variable to another
+    """
     l1 = []
     while inp[pointer]!='':
         l1.append(inp[pointer].split())
         pointer+=1
         if pointer==len(inp):
             break
-    for i in range(len(l1)):
-        for j in range(len(l1[i])):
+    for i in enumerate(l1):
+        for j in enumerate(l1[i]):
             l1[i][j] = int(l1[i][j])
     return l1, pointer
 
 def convert(src, dst_list):
+    """
+    Traversal of list to find required destination. If not found, return original
+    """
     for i in dst_list:
         if i[1]<=src<=i[1]+i[2]:
-            return (i[0]+(src-i[1]))
+            return i[0]+(src-i[1])
     return src
 
-main()
+if __name__ == '__main__':
+    main()
