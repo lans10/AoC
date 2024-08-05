6ea0e62aa0b95cc8ac2c4652071a5982da0eefde
lans10 <123293870+lans10@users.noreply.github.com>
2024-08-05 11:03:58 -0400

    Update day8.py

--- day8.py
+++ day8.py
@@ -5,7 +5,7 @@ def main():
     """
     Part 1 solves for number of moves to move from AAA to ZZZ
     """
-    filename = "input"
+    filename = "sample"
     with open(filename, "r", encoding="utf-8") as f:
         inp = f.readlines()
         moves =inp.pop(0).split()[0]
@@ -17,9 +17,8 @@ def main():
             line = line[1].split()
             value = (line[0][1:4], line[1][:3])
             nodes[key]=value
-    print(moves)
-    print(nodes)
     solve_part1(moves, nodes)
+    solve_part2(moves, nodes)
 
 def solve_part1(moves, nodes):
     """
@@ -30,6 +29,8 @@ def solve_part1(moves, nodes):
     """
     count = 0
     curr = 'AAA'
+    if 'AAA' not in nodes:
+        return 0
     while curr != 'ZZZ':
         for c in moves:
             if c == "L":
@@ -38,5 +39,39 @@ def solve_part1(moves, nodes):
                 curr = nodes[curr][1]
             count += 1
     print(count)
+    
+def solve_part2(moves, nodes):
+    """
+    Solves part 2 by running a loop starting with every node that ends with A,
+    until they end with Z. Follows instructions from variable moves onto nodes.
+    moves: String of moves (L for left, R for right. E.g. "LRLRLR")
+    nodes: Dictionary of nodes ({'AAA':('BBB','CCC')})
+    """
+    count = 0
+    targets = []
+    for key, _ in nodes.items():
+        if key[-1] == 'A':
+            targets.append(key)
+    while not target_helper(targets):
+        for curr in targets:
+            for c in moves:
+                if c == "L":
+                    curr = nodes[curr][0]
+                else:
+                    curr = nodes[curr][1]
+                count+=1
+                if target_helper(targets):
+                    break
+    print(count)
+
+def target_helper(l1):
+    """
+    Just checks if all targets are __Z
+    """
+    for i in l1:
+        if i[-1]!='Z':
+            return False
+    return True
+
 if __name__ == '__main__':
     main()
