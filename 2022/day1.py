c8617ad87d1891e69737c27c566b1a94995aa63b
lans10 <123293870+lans10@users.noreply.github.com>
2024-09-03 16:16:52 -0400

    Update day1.py

--- 2022/day1.py
+++ 2022/day1.py
@@ -2,19 +2,22 @@
 Day 1 of Advent of Code 2022
 """
 def main():
+"""
+Uses white space as a separator
+"""
     with open("input", "r", encoding="utf-8") as f:
-            inp = f.readlines()
-    currentSum=0
+        inp = f.readlines()
+    current_sum=0
     calories=[]
     for line in inp:
         if line!="\n":
-            currentSum+=int(line)
+            current_sum+=int(line)
         else:
-            calories.append(currentSum)
-            currentSum=0
-    sortedCals = sorted(calories)
-    print("Highest calories: "+ str(sortedCals[-1]))
-    print("Top 3 calories:   "+ str(sortedCals[-1]+sortedCals[-2]+sortedCals[-3]))
+            calories.append(current_sum)
+            current_sum=0
+    sorted_cals = sorted(calories)
+    print("Highest calories: "+ str(sorted_cals[-1]))
+    print("Top 3 calories:   "+ str(sorted_cals[-1]+sorted_cals[-2]+sorted_cals[-3]))
 
 if __name__ == '__main__':
     main()
