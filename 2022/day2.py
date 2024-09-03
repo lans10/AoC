56656d721a93f550a5eab971dd39028143fe2d76
lans10 <123293870+lans10@users.noreply.github.com>
2024-09-03 16:59:00 -0400

    Create day2.py

---
+++ 2022/day2.py
@@ -0,0 +1,39 @@
+"""
+Day 2 of Advent of Code 2022
+"""
+from copy import deepcopy as copy
+def main():
+    """
+    RPS
+    """
+    with open("input", "r", encoding="utf-8") as f:
+        inp = f.readlines()
+    rule = {}
+    hand = [None]*7
+    rps = ['',"X","Y","Z"]
+    rule = {"A":copy(hand),"B":copy(hand),"C":copy(hand)}
+    # X rock Y paper Z scissors
+    # A rock
+    rule["A"][0]="Z"
+    rule["A"][3]="X"
+    rule["A"][6]="Y"
+    # B paper
+    rule["B"][0]="X"
+    rule["B"][3]="Y"
+    rule["B"][6]="Z"
+    # C scissors
+    rule["C"][0]="Y"
+    rule["C"][3]="Z"
+    rule["C"][6]="X"
+    
+    points = 0
+    for line in inp:
+        line = line.split()
+        point = rule[line[0]].index(line[1]) + rps.index(line[1])
+        points += point
+    print("Part 1: "+str(points))
+
+    #not good structure for p2
+        
+if __name__ == '__main__':
+    main()
