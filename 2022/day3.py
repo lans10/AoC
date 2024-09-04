6f5ea9439e472506d2c79083ed1d48e18b741859
lans10 <123293870+lans10@users.noreply.github.com>
2024-09-04 12:08:07 -0400

    Update day3.py

--- 2022/day3.py
+++ 2022/day3.py
@@ -11,14 +11,23 @@ def main():
     total = 0
     for line in inp:
         left,right =list(line[:len(line)//2]), list(line[len(line)//2:].strip())
-        common = list(set(left) & set(right))[0]
-        common = ord(common)
+        common = ord(list(set(left) & set(right))[0])
         if common>96:
             common-=96
         else:
             common-=38
         total += common
     print("Part 1: "+str(total))
-        
+    total = 0
+    for i in range(0,len(inp),3):
+        a,b,c = list(inp[i].strip()),list(inp[i+1].strip()),list(inp[i+2].strip())
+        common = ord(list(set(a) & set(b) & set(c))[0])
+        if common>96:
+            common-=96
+        else:
+            common-=38
+        total += common
+    print("Part 2: "+str(total))
+
 if __name__ == '__main__':
     main()
