3e3725d89fc657400c2ee99a121eb268d0d9d552
lans10 <123293870+lans10@users.noreply.github.com>
2024-09-05 13:44:11 -0400

    Update day6.py

--- 2022/day6.py
+++ 2022/day6.py
@@ -8,12 +8,19 @@ def main():
     with open("input", "r", encoding="utf-8") as f:
         inp = f.read()
     read_text = ''
+    part1_done = None
     for i,v in enumerate(inp):
         if len(read_text)>=4:
             check = read_text[len(read_text)-3:]+v
             if len(set(check))==len(check):
-                print("Part 1:"+str(i+1))
-                break
+                if part1_done is None:
+                    print("Part 1:"+str(i+1))
+                    part1_done = True
+                if len(read_text)>=14:
+                    check = read_text[len(read_text)-13:]+v
+                    if len(set(check))==len(check):
+                        print("Part 2:"+str(i+1))
+                        break
         read_text+=v
 if __name__ == '__main__':
     main()
