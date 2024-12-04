3d67e63745c515e83740eb22e798f38764c99610
lans10 <123293870+lans10@users.noreply.github.com>
2024-12-04 15:25:48 -0500

    Update main.rs

--- 2024/day1/main.rs
+++ 2024/day1/main.rs
@@ -1,17 +1,12 @@
 use day1::Config;
-use day1::read_lines;
+use day1::parse_part1;
+use day1::parse_part2;
 
 fn main(){
+	println!("Day 1, Advent of Code 2024");
     //let filename = "./debug.txt";
     let filename = "./input.txt";
-    let config = Config::new(&filename);
-    parse_part1(config.v1, config.v2);
-}
-
-fn parse_part1(v1:&Vec<i32>, v2:&Vec<i32>){
-    let mut sum = 0;
-    for n in 0..v1.len(){
-        sum += i32::abs(v1[n]-v2[n]);
-    }
-    println!("Part 1:\nSum is {}", sum);
+    let config = Config::new(&filename.to_string());
+    parse_part1(&config.v1, &config.v2);
+	parse_part2(&config.v1, &config.v2_sum);
 }
