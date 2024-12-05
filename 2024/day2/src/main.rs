942bd4fcb4ef3bfec48403a91ab5befd8322e66b
lans10 <123293870+lans10@users.noreply.github.com>
2024-12-04 21:28:58 -0500

    Added day 2

---
+++ 2024/day2/src/main.rs
@@ -0,0 +1,8 @@
+use day2::parse_file;
+
+fn main() {
+    println!("Day 2, Advent of Code 2024");
+    //let filename = "./debug.txt";
+    let filename = "./input.txt";
+	parse_file(&filename.to_string());
+}
